23ef8dea9e7eacd33206270a2021784f90aaa249
thanush <thanushsiva06@gmail.com>
2022-01-31 22:06:20 +0530

    updated

--- js/hidemenu.js
+++ js/hidemenu.js
@@ -1,9 +1,13 @@
 const hamburger = document.querySelector(".ham");
 const hmenu = document.querySelector(".hidemenu");
 const close = document.querySelector(".close");
+const bdover = document.querySelector("body");
+
 hamburger.addEventListener('click', e =>{
     hmenu.classList.add("show");
+    bdover.style.overflow="hidden";
 });
 close.addEventListener('click', e =>{
     hmenu.classList.remove("show");
+    bdover.style.overflow="";
 })
